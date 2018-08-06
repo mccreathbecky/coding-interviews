075c8b4461600865a435f08ea6da65db3b6e5499
mccreathbecky <mccreathbecky@gmail.com>
2018-08-06 15:01:42 +1000

    Added Iterative Implementation of binary search

--- treesNgraphs/binarySearch.c
+++ treesNgraphs/binarySearch.c
@@ -7,6 +7,14 @@
 
 #define ISDEBUG 1
 
+
+/**
+ * Uses a Recursive Binary Search to try and locate a number in a sorted array
+ * @params {int []} arr A sorted array to search within
+ *          {int} key The value to search for
+ *          {int} size The size of the array
+ * @returns {bool} True if the key was found
+ */
 bool binarySearchRecursive(int arr[], int key, int left, int right){
     if (left > right){
         return false;
@@ -27,17 +35,53 @@ bool binarySearchRecursive(int arr[], int key, int left, int right){
 }
 
 
+/**
+ * Uses an Iterative Binary Search to try and locate a number in a sorted array
+ * @params {int []} arr A sorted array to search within
+ *          {int} key The value to search for
+ *          {int} size The size of the array
+ * @returns {bool} True if the key was found
+ */
+bool binarySearchIterative(int arr[], int key, int size){
+    if (size < 1){
+        return false;
+    }
+
+    int left = 0;
+    int right = size-1;
+    int mid = left + (right-left)/2;
+
+    while (left <= right){
+
+        mid = left + (right-left)/2;
+        if(ISDEBUG) printf("\nLeft: %d, Right: %d, Mid is: %d", left, right, mid);
+
+        if(arr[mid] == key){
+            if(ISDEBUG) printf("\n%d is in array!",key);
+            return true;
+        } else if (arr[mid] > key){
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+
+    }
+
+    return false;
+}
+
+
 int main(void){
     int sampleArray[] = {1,2,7,11,14,22,79,103};
     bool inArray = false;
 
     if(ISDEBUG) printf("\nIs 7 in array?");
-    inArray = binarySearchRecursive(sampleArray, 7, 0, 7);
+    inArray = binarySearchIterative(sampleArray, 7, 8);
     if(!inArray)
         if(ISDEBUG) printf("\nSadly, no.");
 
     if(ISDEBUG) printf("\nIs 150 in array?");
-    inArray = binarySearchRecursive(sampleArray, 150, 0, 7);
+    inArray = binarySearchIterative(sampleArray, 150, 8);
     if(!inArray)
         if(ISDEBUG) printf("\nSadly, no.");
 
